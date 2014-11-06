2b6f31ff76129135c91da74c3f0cf56306e5b94e
青柳　恭義 <uu070970@CA0250.local>
2014-11-06 14:39:54 +0900

    textarea, textbox

--- sample/app.js
+++ sample/app.js
@@ -2,9 +2,27 @@
 
 var App = function () {
 
+    var ta1 = new FormBase.TextInput({
+	el: "#ta1",
+	text: "default"
+    });
+    
+    var ta2 = new FormBase.TextInput({
+	el: "#ta2",
+	text: "initial"
+    });
+    
     var btn1 = new FormBase.Button({
 	el: "#btn1",
-	label: "Button1",
+	text: "Button1",
+	onClick: function () {
+	    console.log("Clicked");
+	}
+    });
+
+    var btn2 = new FormBase.Button({
+	el: "#btn2",
+	text: "Button2",
 	onClick: function () {
 	    console.log("Clicked");
 	}
@@ -12,11 +30,16 @@ var App = function () {
 
     setTimeout(function () {
 	btn1.disable();
-	btn1.label("Button1: disabled");
+	btn1.text("Button1: disabled");
     }, 5000);
 
     setTimeout(function () {
 	btn1.enable();
-	btn1.label("Button1");
+	btn1.text("Button1");
     }, 10000);
+
+    setTimeout(function () {
+	ta1.reset();
+	ta2.reset();
+    }, 12000);
 };
--- src/form_base.js
+++ src/form_base.js
@@ -7,6 +7,51 @@
      */
     var AbstractForm = Backbone.View.extend({
 
+	events: {
+	    "click": "_onClick"
+	},
+
+	/**
+	 * Replace callback function of on click event
+	 *
+	 * @param callback callback function
+	 */
+	setOnClick: function (callback) {
+	    this._onClick = callback;
+	},
+
+	/**
+	 * Initialize common objects.
+	 */
+	_init: function (options) {
+	    var msg;
+	    // el (required)
+	    if (!options.el) {
+		msg = this._PREFIX + "el is NOT set.";
+		new Error(msg);
+	    }
+	    this.el = options.el;
+
+	    // on click
+	    this.setOnClick(options.onClick);
+	},
+
+	/**
+	 * Returns tag type.
+	 * @example "INPUT", "BUTTON" ...
+	 */
+	_getTagType: function () {
+	    return this.$el[0].tagName;
+	},
+	
+	/**
+	 * default behavior of click event (throws Error!)
+	 */ 
+	_onClick: function () {
+	    var msg = this._PREFIX + "Click event is NOT implemented yet.";
+	    throw new Error(msg);
+	},
+
 	/**
 	 * enable disabled element
 	 */
@@ -19,7 +64,7 @@
 	 */
 	disable: function () {
 	    this.$el.attr("disabled", "disabled");
-	}	
+	}
     });
 
     /**
@@ -27,70 +72,103 @@
      */
     var Button = AbstractForm.extend({
 	
-	PREFIX: "FormBase.Button: ",
+	_PREFIX: "FormBase.Button: ",
 
 	/**
 	 * initialize button
 	 *
 	 * @param options.el id (required)
-	 * @param options.label label text of this button.
+	 * @param options.text label text of this button.
 	 * @param options.onClick callback function of click event (required)
 	 */
 	initialize: function (options) {
-	    var msg;
-	    if (!options.el) {
-		msg = PREFIX + "el is NOT set.";
-		new Error(msg);
-	    }
-	    this.el = options.el;
-
-	    this.labelName = options.label;
-	    this.label(this.labelName);
-	
-	    if (options.onClick) {
-		this.onClick = options.onClick;
-	    }
+	    this._init(options);
+	    //
+	    this._text = options.text;
+	    this.text(this._text);
 	},
     
-	events: {
-	    "click": "onClick"
-	},
-
 	/**
-	 * default behavior of click event (throws Error!)
-	 */ 
-	onClick: function () {
-	    var msg = PREFIX + "Click event is NOT implemented yet.";
-	    throw new Error(msg);
-	},
-
-	/**
-	 * Get label text if argument is null.
-	 * Set label if argument is exist.
+	 * Get text if argument is null.
+	 * Set text if argument exists.
 	 *
-	 * @param label label text
+	 * @param text label text
 	 */
-	label: function (label) {
-	    if (!label) {
-		return this.labelName;
+	text: function (str) {
+	    if (!str) {
+		return this._text;
 	    }
-	    this.labelName = label;
-	    this.$el.text(this.labelName);
+	    var tagType = this._getTagType();
+
+	    switch (tagType) {
+	    case "INPUT":
+		var type = this.$el.attr("type");
+		switch (type) {
+		case "button":
+		    this.$el.val(str);    // when <input type="button">
+		    break;
+		default:
+		    throw new Error("text(): Invalid type (" + type + ")");
+		}
+		break;
+	    case "BUTTON":
+		this.$el.text(str); // when <button>
+		break;
+	    default:
+		throw new Error("text(): Invalid tagType (" + tagType + ")");
+	    }
+	    this._text = str
+	}
+    });
+
+    /**
+     * Text area or text box
+     */
+    var TextInput = AbstractForm.extend({
+	
+	_PREFIX: "FormBase.TextArea: ",
+
+	/**
+	 * initialize button
+	 *
+	 * @param options.el id (required)
+	 * @param options.text default text
+	 * @param options.onClick callback function of click event
+	 */
+	initialize: function (options) {
+	    this._init(options);
+	    //
+	    this._defaultText = options.text;
+	    this._text = this._defaultText;
+	    this.text(this._text);
+	},
+    
+	/**
+	 * Get text if the argument is null.
+	 * Set text if the argument exists.
+	 *
+	 * @param str label text
+	 */
+	text: function (str) {
+	    if (!str) {
+		return this._text;
+	    }
+	    this.$el.val(str);
+	    this._text = str;
 	},
 
 	/**
-	 * Replace callback function of on click event
-	 *
-	 * @param callback callback function
+	 * reset to default text.
 	 */
-	setOnClick: function (callback) {
-	    this.onClick = callback;
+	reset: function () {
+	    this.text(this._defaultText);
 	}
     });
 
     // Aggregate functions to FormBase
     var FormBase = function () {};
     FormBase.Button = Button;
+    FormBase.TextInput = TextInput;
 	
     // export FormBase
     window.FormBase = FormBase;
